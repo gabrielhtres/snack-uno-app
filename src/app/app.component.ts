20024d3d903197285be3461ba00baf3db53cc55b
Gabriel <gabrielhtres@gmail.com>
2022-04-25 14:35:24 -0300

    Ajustados itens do menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/home', icon: 'home' },
+    { title: 'Meu Perfil', url: '/folder/meu-perfil', icon: 'person' },
+    { title: 'Meus Pedidos', url: '/folder/meus-pedidos', icon: 'basket' },
+    { title: 'Meus Cartões', url: '/folder/meus-cartoes', icon: 'card' },
+    { title: 'Meus Locais', url: '/folder/meus-locais', icon: 'location' },
+    { title: 'Configurações', url: '/folder/configuracoes', icon: 'construct' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
