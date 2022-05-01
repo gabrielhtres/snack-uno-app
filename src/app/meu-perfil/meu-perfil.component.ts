21c6c9bfe602d2573d2668f2181ca4d1994867fa
AnaSitec001 <analaura@sitecbr.com.br>
2022-05-01 01:53:28 -0300

    Alterações para deixar opções dinamicas de perfil

--- src/app/meu-perfil/meu-perfil.component.ts
+++ src/app/meu-perfil/meu-perfil.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meu-perfil.component.scss'],
 })
 export class MeuPerfilComponent implements OnInit {
+  public dadosPerfil = [
+    { titulo: 'Nome', dado: 'Gabriel', icon: 'person' },
+    { titulo: 'E-mail', dado: 'gabriel@gabriel.com', icon: 'mail' },
+    { titulo: 'Celular', dado: '(49) 99999-9999', icon: 'call' },
+    { titulo: 'CPF', dado: '***.***.***-**', icon: 'card' },
+    { titulo: 'Data de Nascimento', dado: '19/09/2000', icon: 'calendar' }
+  ];
 
   constructor() { }
 
