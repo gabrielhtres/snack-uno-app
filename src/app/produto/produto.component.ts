bd7d2e4037056777e1066eb78aca8fe12cfc65f6
AnaSitec001 <analaura@sitecbr.com.br>
2022-05-10 21:24:33 -0300

    Pequenas alterações feitas no componente Produto

--- src/app/produto/produto.component.ts
+++ src/app/produto/produto.component.ts
@@ -12,13 +12,11 @@ export class ProdutoComponent implements OnInit {
   public id: string = ''
   public restaurante: string = ''
 
-  public dadosProdutos = [
+  public Produtos = [
     [
       { id: '1', img: '../../assets/pizza.jpg', restaurante: 'Hangar', produto: 'Pizza', descricao: 'Pizza de Calabresa', preco: 'R$ 5.00' },
       { id: '2', img: '../../assets/pastel.webp', restaurante: 'Hangar', produto: 'Pastel', descricao: 'Pastel de Frango', preco: 'R$ 4.00' },
-      { id: '3', img: '../../assets/cafe.jpg', restaurante: 'Hangar', produto: 'Café', descricao: 'Café Preto com Açúcar', preco: 'R$ 3.00' }  
-    ],
-    [
+      { id: '3', img: '../../assets/cafe.jpg', restaurante: 'Hangar', produto: 'Café', descricao: 'Café Preto com Açúcar', preco: 'R$ 3.00' },
       { id: '1', img: '../../assets/pizza.jpg', restaurante: 'Da Tia', produto: 'Pizza', descricao: 'Pizza de Calabresa', preco: 'R$ 5.00' },
       { id: '2', img: '../../assets/pastel.webp', restaurante: 'Da Tia', produto: 'Pastel', descricao: 'Pastel de Frango', preco: 'R$ 4.00' },
       { id: '3', img: '../../assets/cafe.jpg', restaurante: 'Da Tia', produto: 'Café', descricao: 'Café Preto com Açúcar', preco: 'R$ 3.00' }  
@@ -32,7 +30,7 @@ export class ProdutoComponent implements OnInit {
     this.id = this.rotaAtiva.snapshot.params.id
     this.restaurante = this.rotaAtiva.snapshot.params.restaurante
 
-
+    
   }
 
 }
