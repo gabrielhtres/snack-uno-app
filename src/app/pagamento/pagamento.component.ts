0f7fc4d5c5701e9230a40af092bf8095ccef4582
Gabriel <gabrielhtres@gmail.com>
2022-05-17 21:45:11 -0300

    Iniciada configuração da tela de pagamento

--- src/app/pagamento/pagamento.component.ts
+++ src/app/pagamento/pagamento.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagamentoComponent implements OnInit {
 
+  public alertOptions = {
+    cssClass: 'alertClass',
+    header: 'Pagamento',
+  }
+
   constructor() { }
 
   ngOnInit() {}
