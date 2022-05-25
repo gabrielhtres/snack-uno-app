178b57a65f86cc81bfa39fc9a09970f4e81bb144
Gabriel <gabrielhtres@gmail.com>
2022-05-25 15:35:03 -0300

    Criada model de restaurante

---
+++ src/shared/restaurante.model.ts
@@ -0,0 +1,10 @@
+export class Restaurante {
+
+    public id: number
+    public nome: string
+    public formasPagamento: number[]
+    public localizacao: string
+    
+    constructor() {}
+    
+}
