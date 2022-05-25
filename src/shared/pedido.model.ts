e403b8c14efce675bd56485d74a810ce11b6c876
Gabriel <gabrielhtres@gmail.com>
2022-05-25 15:29:48 -0300

    Criada model de pedido

---
+++ src/shared/pedido.model.ts
@@ -0,0 +1,10 @@
+export class Pedido {
+    
+    public id: number
+    public idRestaurante: number
+    public idUsuario: number
+    public data: string
+    public valorTotal: number
+
+    constructor() {}
+}
