2b96056144c38ea259a4c0e1aa89c610ee9dc2d8
AnaSitec001 <analaura@sitecbr.com.br>
2022-06-02 21:06:05 -0300

    Ajustada lista com espaço a mais

--- src/app/minha-cesta/minha-cesta.component.ts
+++ src/app/minha-cesta/minha-cesta.component.ts
@@ -2,31 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { ProdutoPedido } from 'src/shared/produto-pedido.model';
 import { Produto } from 'src/shared/produto.model';
 import { CestaService } from '../cesta.service';
+import { ProdutosService } from '../produtos.service';
+import { RestauranteService } from '../restaurante.service';
 
 @Component({
   selector: 'app-minha-cesta',
   templateUrl: './minha-cesta.component.html',
   styleUrls: ['./minha-cesta.component.scss'],
+  providers: [ ProdutosService, RestauranteService ]
 })
 export class MinhaCestaComponent implements OnInit {
 
   public produtosCesta: ProdutoPedido[]
 
-  public dadosCesta = [
-    { produto: 'Pizza', descricao: 'Pizza de Calabresa', valor: '5.00', restaurante: 'Hangar', quantidade: '1', src: 'https://picsum.photos/200/200' },
-    { produto: 'Café', descricao: 'Café Preto Sem Açucar', valor: '3.00', restaurante: 'Da Tia', quantidade: '1', src: 'https://picsum.photos/200/200' }
-  ];
+  // public dadosCesta = [
+  //   { produto: 'Pizza', descricao: 'Pizza de Calabresa', valor: '5.00', restaurante: 'Hangar', quantidade: '1', src: 'https://picsum.photos/200/200' },
+  //   { produto: 'Café', descricao: 'Café Preto Sem Açucar', valor: '3.00', restaurante: 'Da Tia', quantidade: '1', src: 'https://picsum.photos/200/200' }
+  // ];
 
-  public dadosPedidos = [
-    { pedido: '#12', restaurante: 'Hangar', src: 'https://picsum.photos/200/200', data: '28/04/2022', valor: '7.00' },
-    { pedido: '#15', restaurante: 'Da Tia', src: 'https://picsum.photos/200/200', data: '29/04/2022', valor: '12.00' },
-  ];
+  // public dadosPedidos = [
+  //   { pedido: '#12', restaurante: 'Hangar', src: 'https://picsum.photos/200/200', data: '28/04/2022', valor: '7.00' },
+  //   { pedido: '#15', restaurante: 'Da Tia', src: 'https://picsum.photos/200/200', data: '29/04/2022', valor: '12.00' },
+  // ];
 
-  constructor(private cestaService: CestaService) { }
+  constructor(
+    private cestaService: CestaService
+    ) { }
 
   ngOnInit() {
     this.produtosCesta = this.cestaService.cesta.produtos
-    console.log('Teste', this.produtosCesta)
+    
+    console.log(this.produtosCesta)
   }
 
 }
