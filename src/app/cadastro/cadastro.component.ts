c03ef895f457ac30d0ebad32966102b752722d36
AnaSitec001 <analaura@sitecbr.com.br>
2022-06-26 16:56:00 -0300

    Criada tela de cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent implements OnInit {
+
+  public formularioCadastro: FormGroup = new FormGroup({
+    'nome': new FormControl(null),
+    'email': new FormControl(null),
+    'dataNascimento': new FormControl(null),
+    'senha': new FormControl(null),
+    'cpf': new FormControl(null),
+    'telefone': new FormControl(null)
+  })
 
   constructor() { }
 
