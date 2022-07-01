3cf040ff1c9467531c88821a9073a19395828e9d
Gabriel <gabrielhtres@gmail.com>
2022-07-01 09:43:02 -0300

    Ngx-mask desinstalado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { NgxMaskModule, IConfig } from 'ngx-mask';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -16,7 +15,7 @@ import { Autenticacao } from './autenticacao.service';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, HttpClientModule ,IonicModule.forRoot(), AppRoutingModule, NgxMaskModule.forRoot({ validation: false })],
+  imports: [BrowserModule, HttpClientModule ,IonicModule.forRoot(), AppRoutingModule ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Autenticacao, CestaService ],
   bootstrap: [AppComponent],
 })
